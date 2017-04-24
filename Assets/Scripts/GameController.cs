b3f7815e50cbdbdf14dc81adf67c94a3228164f3
Matt Martucciello <kassanu@gmail.com>
2017-04-23 21:06:51 -0400

    Added Score

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     private bool isGameOver, restart;
     public GameObject[] enemyTypes;
-    public float minSpawnWait, maxSpawnWait, startWait, lastSpawn;
+    public float minSpawnWait, maxSpawnWait, startWait, lastSpawn, timeAlive;
     public int minEnemySpawn, maxEnemySpawn;
     public Player player;
     public Camera mainCamera;
     public GameObject blackScreen;
+    public Text timeText;
 
     // Use this for initialization
     void Start () {
         this.isGameOver = false;
         this.restart = false;
+        this.timeAlive = 0;
 	}
 	
 	// Update is called once per frame
@@ -29,16 +32,21 @@ public class GameController : MonoBehaviour {
                 Destroy(enemy);
             }
             
-        if (Input.GetKeyDown(KeyCode.R)) {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if (Input.GetKeyDown(KeyCode.R)) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
+
+            this.timeText.text = "You survived for " + (int)timeAlive + " seconds.";
+
             this.player.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
             this.blackScreen.SetActive(true);
         } else {
+            this.timeAlive += Time.deltaTime;
+
             if (this.startWait <= 0) {
                 if (Time.time > this.lastSpawn + Random.Range(this.minSpawnWait, this.maxSpawnWait)) {
                     this.lastSpawn = Time.time;
-                    int eNum = Random.Range(this.minEnemySpawn, this.maxEnemySpawn);
+                    int eNum = Random.Range(this.minEnemySpawn, this.maxEnemySpawn+1);
 
                     for (int i = 0; i < eNum; i++) {
                         float rNum = Random.Range(0.0f, 10.0f);
